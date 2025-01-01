e76594587547ef96a890af5bfd48b01cf1c9741a
Nobe Ananda <anobe32@gmail.com>
2025-01-01 11:19:51 +0700

    Added EF Core and Scaffolded Database

---
+++ BookStoreApp.API/Data/BookStoreDbContext.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApp.API.Data;
+
+public partial class BookStoreDbContext : DbContext
+{
+    public BookStoreDbContext()
+    {
+    }
+
+    public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+    public virtual DbSet<CLassTable1> CLassTable1 { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Author__3214EC071BF451AA");
+
+            entity.ToTable("Author");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Bio).HasMaxLength(250);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__BookS__3214EC079C4F1D5D");
+
+            entity.ToTable("BookS");
+
+            entity.HasIndex(e => e.Isbn, "UQ__BookS__447D36EABDB95E61").IsUnique();
+
+            entity.Property(e => e.Image)
+                .HasMaxLength(50)
+                .HasColumnName("image");
+            entity.Property(e => e.Isbn)
+                .HasMaxLength(50)
+                .HasColumnName("ISBN");
+            entity.Property(e => e.Price)
+                .HasColumnType("decimal(18, 0)")
+                .HasColumnName("price");
+            entity.Property(e => e.Summary)
+                .HasMaxLength(50)
+                .HasColumnName("summary");
+            entity.Property(e => e.Title)
+                .HasMaxLength(50)
+                .HasColumnName("title");
+            entity.Property(e => e.Year).HasColumnName("year");
+
+            entity.HasOne(d => d.Author).WithMany(p => p.Books)
+                .HasForeignKey(d => d.AuthorId)
+                .HasConstraintName("FK_BookS_ToTable");
+        });
+    }
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
